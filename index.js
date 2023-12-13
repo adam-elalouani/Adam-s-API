8cd2c8e833a1770fb18aedadb00c697217a14f21
adam-elalouani <adam_elalouani@horacemann.org>
2023-12-13 12:55:12 -0500

    adam

--- index.js
+++ index.js
@@ -9,21 +9,25 @@ app.get('/add',function(req,res){
 })
 
 app.get('/product', function(req,res){
+    console.log(req.body.array)
    const product = res.reduce((accumulator,nums) => acc * num,1);
     res.json({product});
 })
 
 app.get('/evens', function(req,res){
+    console.log(req.body.array)
    const evens = res.filter(num => num % 2 === 0);
     res.json({evens})
 })
 
 app.get('/min', function(req,res){
+    console.log(req.body.array)
    const min = Math.min(...array);
     res.json({min})
 })
 
 app.get('/max', function(req,res){
+    console.log(req.body.array)
    const max = Math.max(...array);
     res.json({max})
 })
